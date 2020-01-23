8447c113058394db9a4f3ed54e08525b2bb537a4
Rwanda S <rwanda.ace@gmail.com>
2020-01-23 20:45:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  var welcomeMessage =  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  return welcomeMessage
   
 }
