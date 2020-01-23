1cbb0d2a7c9738f528335eb1c063b02c838595b4
Rwanda S <rwanda.ace@gmail.com>
2020-01-23 20:26:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(linePosition, custName){
+  var i = 0
   var realLineNumber = 1 + linePosition [i]
   return `Welcome, ${custName}. You are number ${realLineNumber} in line.`
 }
