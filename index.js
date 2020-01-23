77a5b6cb24157a434b53220d69a09928dd3fd4fb
Rwanda S <rwanda.ace@gmail.com>
2020-01-23 20:47:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
   var welcomeMessage =  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
   return welcomeMessage
+}
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length > 0)
+  {
+    var firstPerson = katzDeliLine[0]
+    katzDeliLine.unshift()
+    return katzDeliLine[0]
+  }else{return "There is nobody waiting to be served!"}
+  
   
 }
